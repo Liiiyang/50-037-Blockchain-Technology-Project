096db9d5c25542df491b6ff98fd6be513d31fca8
Tan Li Yang <tanliyang3000@hotmail.com>
2018-10-08 14:09:46 +0800

    Add files via upload

--- blockchain.py
+++ blockchain.py
@@ -19,30 +19,37 @@ class Blockchain:
         genesis_block = Block(0,time.time(),0,10,0)
         genesis_block.hash = genesis_block.getHeaderInJSON()       
         self.chain.append(genesis_block)
+        self.fork[genesis_block.hash] = self.chain
         print("Genesis: " + str(self.chain))
     
     @property
     def last_block(self):
         return self.chain[-1]
 
-    def add(self, block, proof, fork):
+    def add(self, block, proof, fork, position):
         print("Adding")
-        if "Yes" in fork and self.chain[-2]:
-            previous_hash = self.chain[-2].hash
-            self.second_chain = self.chain[:-2]
-            if previous_hash != block.header["prevHeaderHash"]:
-                print("1")
+        if "Yes" in fork:
+            if self.chain[-position]:
+                previous_hash = self.chain[-position].hash
+                self.second_chain = self.chain[:-position]
+                print("Prev: " + previous_hash)
+                print("Current: " + str(block.header["prevHeaderHash"]))
+                if previous_hash != block.header["prevHeaderHash"]:
+                    print("1")
+                    return False
+
+                if not self.validate(block, proof):
+                    print("2")
+                    return False
+
+                block.hash = proof
+                self.second_chain.append(block)
+                self.fork[block.hash] = self.second_chain
+            else:
+                print("Invalid Block")
                 return False
 
-            if not self.validate(block, proof):
-                print("2")
-                return False
-
-            block.hash = proof
-            self.second_chain.append(block)
-            self.fork[block.hash] = self.second_chain
-
-        elif "No" in fork and resolve: 
+        elif "No" in fork and self.resolve(): 
             block.hash = proof
             previous_hash = self.last_block.hash
             print("Prev: " + previous_hash)
@@ -58,7 +65,7 @@ class Blockchain:
             block.hash = proof
             self.chain.append(block)
             self.fork[block.hash] = self.chain
-            print("3")
+            print("Block Added")
         return True
 
     def add_transactions(self, transaction):
@@ -88,7 +95,7 @@ class Blockchain:
                 return blockWithNewNonce
 
     def resolve(self):
-        return max(self.fork.values,key=len)
+        return max(self.fork.values(),key=len)
 
 if __name__ == "__main__":
     with open("transaction_hash.json") as json_file:
@@ -97,5 +104,10 @@ if __name__ == "__main__":
     blockOne = Block(0,time.time(),bc.last_block.hash,10,data)
     proof = bc.proof_of_work(blockOne)
     print(proof)
-    bc.add(blockOne,proof,"No")
+    bc.add(blockOne,proof,"No",0)
+    blockTwo = Block(0,time.time(),bc.chain[-2].hash,10,data)
+    proofTwo = bc.proof_of_work(blockTwo)
+    print(proofTwo)
+    bc.add(blockTwo,proofTwo,"Yes",2)
     print("Total Chain: " + str(bc.chain))
+    print("Forks: " + str(bc.fork.values()))
