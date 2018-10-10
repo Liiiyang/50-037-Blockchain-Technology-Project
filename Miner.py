f32151f54c8333b6fc3c79176a73dd5171655b03
Tan Li Yang <tanliyang3000@hotmail.com>
2018-10-10 13:11:47 +0800

    Add files via upload

--- Miner.py
+++ Miner.py
@@ -1,4 +1,6 @@
 from transaction import Transaction
+from blockchain import Blockchain
+from block import Block
 import time  
 import hashlib 
 from flask import Flask, jsonify, json, request, Response
@@ -24,6 +26,39 @@ def new():
         'Signature': newData
         }
     return jsonify(response), 201
+
+@app.route('/get-header', methods = ['GET'])
+def get_header():
+    list_of_headers = []
+    with open("transaction_hash.json") as json_file:
+        data=json.load(json_file)
+    bc = Blockchain()
+    blockOne = Block(0,time.time(),bc.last_block.hash,10,data)
+    proof = bc.proof_of_work(blockOne)
+    print(proof)
+    bc.add(blockOne,proof,"No",0)
+    blockTwo = Block(0,time.time(),bc.chain[-2].hash,10,data)
+    proofTwo = bc.proof_of_work(blockTwo)
+    print(proofTwo)
+    bc.add(blockTwo,proofTwo,"Yes",2)
+    print("Total Chain: " + str(bc.chain))
+    print("Forks: " + str(bc.fork.values()))
+    print("Resolve: " + str(bc.resolve()))
+    print("Length: " + str(len(bc.resolve())))
+    for block in bc.resolve():
+        print("Block: "+ str(block))
+        for attr, value in block.__dict__.items():
+            if(attr == "header"):
+                print("Header: " + str(value))
+                list_of_headers.append(value)
+    js = json.dumps(list_of_headers)
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http://luisrei.com'
+    return resp
+
+@app.route('/get-proof', methods = ['GET'])   
+def get_proof():
+    pass
 
 from argparse import ArgumentParser  
 parser = ArgumentParser()  
