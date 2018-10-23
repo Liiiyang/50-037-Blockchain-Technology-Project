e6d76bbe60c8739bd40ddb6f7bc5f18cb5a43d34
Tan Li Yang <tanliyang3000@hotmail.com>
2018-10-23 16:20:16 +0800

    Add files via upload

--- Ex4/MinerServer.py
+++ Ex4/MinerServer.py
@@ -1,18 +1,12 @@
-'''
-Reference:
-    https://medium.com/@manivannan_data/how-to-get-data-received-in-flask-request-8ebadc2bb5c6
-'''
-
-# from blockchain import Blockchain
-# from block import Block
+from transaction import Transaction
+from blockchain import Blockchain
+from block import Block
 from flask import Flask, jsonify, json, request, Response,send_from_directory
+from argparse import ArgumentParser  
 import time  
 import hashlib 
 import os
 import pickle
-from argparse import ArgumentParser  
-from blockchain import Blockchain
-from Transaction import Transaction
 
 UPLOAD_DIRECTORY = 'C:/Users/Li Yang/source/repos/BlockchainTechnology50037/BlockchainTechnology50037'  
 app = Flask(__name__) 
@@ -24,16 +18,6 @@ args = parser.parse_args()
 
 # Example command:
 # python3 MinerServer.py -p 5000
-
-# @app.route('/create-transaction', methods = ['GET'])
-# def get_transaction():
-#     data = {
-#        'Transaction'  : '',
-#     }
-#     js = json.dumps(data)
-#     resp = Response(js, status=200, mimetype='application/json')
-#     resp.headers['Link'] = 'http://luisrei.com'
-#     return resp
 
 # requests.post('http://127.0.0.1:{}/create-transaction'.format(myId), json=myTxJSON ,headers=headers)
 # TODO: Done. Transaction can be sent as a string or python object
@@ -47,7 +31,7 @@ def create_transaction():
 
 	# Read request data
     if request.headers['Content-Type'] == 'text/plain':
-	    myTx = request.data
+        myTx = request.data
     elif request.headers['Content-Type'] == 'application/json':
         myTx = request.get_json()
         # newData = request.json
@@ -59,14 +43,12 @@ def create_transaction():
         # 	"amount": newData["amount"],
         # }
         # myTx = Transaction.new(newData["sender"], newData["receiver"], newData["amount"], newData["privateKey"])
-    # print(type(myTx))
-    # txObj = Transaction.from_json(myTx)
-    # print(type(txObj))
+    print(myTx)
     data["AllTransactions"].append(myTx)
 
     # Write JSON file
     with open('pending_transactions.json', 'w') as outfile:
-	    json.dump(data, outfile)
+        json.dump(data, outfile)
     response = {
         'message': 'Transaction has been submitted successfully',
         # 'Signature': newData
@@ -78,18 +60,19 @@ def create_transaction():
 
 
 # TODO: Read from pending_transactions.JSON file
-# requests.get('http://127.0.0.1:{}/read-transactions'.format(myId))
+# requests.get('http://127.0.0.1:{}/read-transaction'.format(myId))
 @app.route('/read-transactions', methods=['GET'])
 def read_transactions():
-    with open('./pending_transactions.json') as f:
-        data = json.load(f)
+    for File in os.listdir("."):
+        if File.endswith(".json"):
+            with open('pending_transactions.json') as f:
+                data = json.load(f)
     resp = data
     # TODO: delete
     # data["AllTransactions"] = []
     # with open('pending_transactions.json', 'w') as outfile:
     #     json.dump(data, outfile)
     return jsonify(resp)
-
 
 # f = requests.get('http://127.0.0.1:{}/read-blockchain'.format(myId))
 # myBlockchain = pickle.loads(f.content)
@@ -116,12 +99,41 @@ def read_block_header():
     depth = int(request.args['depth'])
     with open('./{}/blockchain'.format(args.port), 'rb') as f:
         bc = pickle.load(f)
-    height = len(bc.chain)
-    ls = []
-    for b in bc.chain[height-depth:]:
-        ls.append(b.header)
-    return jsonify(ls)
+        height = len(bc.chain)
+        print(height)
+        ls = []
+        for b in bc.chain:
+            print(b)
+            ls.append(b.header)
+    resp = pickle.dumps(ls)
+    return Response(resp)
 
+@app.route('/read-tx-and-proof', methods = ['GET'])
+def read_tx_proof():
+    # Open blockchain file
+    all_my_transactions = []
+    all_my_txProofs = []
+    length = int(request.args['length'])
+    receiver = request.args['receiver']
+    with open('./{}/blockchain'.format(args.port), 'rb') as f:
+        bc = pickle.load(f)
+        depthOfBlocks = bc.chain[length:]
+        for currentBlock in depthOfBlocks:
+            print("Tx: " + str(currentBlock.transactions))
+            for tx in currentBlock.transactions:
+                print("tx: " + str(tx))
+                #Or Bob's public key
+                if Transaction.from_json(tx)["receiver"] == receiver: 
+                    all_my_transactions.append(tx)             
+            for myTx in all_my_transactions:
+                txProof = currentBlock.get_merkleProofs(myTx)
+                all_my_txProofs.append(txProof)
+    data = {
+        "Transactions": all_my_transactions,
+        "Proofs": all_my_txProofs
+        }
+    resp = pickle.dumps(data)
+    return Response(resp)
 
 # For checking who found a new nonce
 # TODO: Done
@@ -157,39 +169,6 @@ def read_blocks_from_winner():
     resp = pickle.dumps(list_of_blocks)
     return Response(resp)
 
-# Deprecated
-# @app.route('/get-header', methods = ['GET'])
-# def get_header():
-#     list_of_headers = []
-#     with open("transaction_hash.json") as json_file:
-#         data=json.load(json_file)
-#     bc = Blockchain.new()
-#     blockOne = Block(time.time(),bc.last_block.getHeaderInHash(),10,['123','456'])
-#     proof = bc.proof_of_work(blockOne)
-#     print(proof)
-#     bc.add(blockOne,proof,"No",0)
-
-#     blockTwo = Block(time.time(),bc.chain[-2].getHeaderInHash(),10,['abc','def'])
-#     proofTwo = bc.proof_of_work(blockTwo)
-#     print(proofTwo)
-#     bc.add(blockTwo,proofTwo,"Yes",2)
-#     print("Total Chain: " + str(bc.chain))
-#     print("Forks: " + str(bc.fork.values()))
-#     print("Resolve: " + str(bc.resolve()))
-#     print("Length: " + str(len(bc.resolve())))
-#     for block in bc.resolve():
-#         print("Block: "+ str(block))
-#         for attr, value in block.__dict__.items():
-#             if(attr == "header"):
-#                 print("Header: " + str(value))
-#                 list_of_headers.append(value)
-#     js = json.dumps(list_of_headers)
-#     resp = Response(js, status=200, mimetype='application/json')
-#     resp.headers['Link'] = 'http://luisrei.com'
-#     return resp
-
-
-
 # Deprecated: Use local write instead
 # @app.route('/update-blockchain', methods=['POST'])
 # def update_blockchain():
@@ -212,5 +191,4 @@ def read_blocks_from_winner():
 #     else:
 #         return "415 Unsupported Media Type"
 
-  
 app.run(host=args.host, port=args.port, debug=True)
---
+++ Ex4/SPVClient.py
@@ -0,0 +1,94 @@
+'''
+Reference:
+	http://blog.luisrei.com/articles/flaskrest.html
+
+'''
+
+'''
+Design and implement an SPVClient class. SPV clients should implement a simple 
+SPV logic, i.e., they should:
+
+	1. have their key pairs associated
+	2. be able to receive block headers (not full blocks)
+	3. be able to receive transactions (with their presence proofs) and verify them
+	4. be able to send transactions
+
+Integrate your implementation with your simulator from the previous exercise. Test your implementation.
+
+'''
+
+import requests
+import json
+# from requests.auth import HTTPBasicAuth
+from transaction import Transaction
+from ecdsa import SigningKey, VerifyingKey, NIST192p
+import hashlib
+import argparse
+import pickle
+
+vk = VerifyingKey.from_pem(open("vk.pem").read())
+sk = SigningKey.from_pem(open("sk.pem").read())
+sk_string = sk.to_string()
+
+new_tx = Transaction.new("you", "me", 5000, sk_string,"First Transaction")
+hash_tx = hashlib.sha256(str(new_tx).encode()).hexdigest()
+print(hash_tx)
+
+# Instantiate services
+class SPVClient():
+    def __init__(self,myid):
+        path = "./"+ str(myid) + "/"
+        vk = VerifyingKey.from_pem(open(path+"vk.pem").read())
+        sk = SigningKey.from_pem(open(path+"sk.pem").read())
+
+        sk_string = sk.to_string()
+        vk_string = vk.to_string()
+        self.verifyKey = vk_string
+        self.signingKey = sk_string
+
+           
+    def getBlockHeader(self, MinerId, depth):
+        return requests.get('http://127.0.0.1:{}/read-block-header'.format(MinerId), params={'depth': depth})
+    
+    def sendTransaction(self,MinerId,rcv,snd, amt, cmmt):
+        new_tx = Transaction.new(rcv, snd, amt, self.signingKey, cmmt)
+        tx_string = new_tx.to_json()
+        data = {
+            "Transaction" : tx_string
+            }
+        return requests.post('http://127.0.0.1:{}/create-transaction'.format(MinerId), json=data)
+
+    def receiveTransactionASndProofs(self,MinerId,length,receiver):
+        return requests.get('http://127.0.0.1:{}/read-tx-and-proof'.format(MinerId), params={'length': length, 'receiver': receiver})
+
+# TODO: Local read private public keys
+
+# TODO: Get all transactions and proofs
+    
+# TODO: Requests GET block headers
+
+# TODO: While and await for commands (Send transactions)
+# while
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', type=int, dest='id',
+        help='Name of the folder for the keys')
+
+    args = parser.parse_args()
+
+    if args.id == None:
+        print("Please enter the folder id")
+    else:
+        print(SPVClient(args.id).sendTransaction(5000,"you", "me", 5000,"First Transaction"))
+
+        msg = SPVClient(args.id).receiveTransactionASndProofs(5000, 1, "5000").content
+        msg = pickle.loads(msg)
+        print(msg)
+
+        msg1 = SPVClient(args.id).getBlockHeader(5000,0).content
+        msg1 = pickle.loads(msg1)
+        print(msg1)
+
+
+
